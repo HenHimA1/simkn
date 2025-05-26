df47aee20364eaadf8e39e5f6d1b73b3a01180e9
HenHimA1 <hendrahima1@gmail.com>
2025-05-27 06:35:22 +0700

    update select

--- simkn/settings.py
+++ simkn/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'simkn.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'examination' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
